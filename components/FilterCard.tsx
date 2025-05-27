63eb064b4cc309fc1b3f10abc0c7eb6ed4999b1e
nadesh52 <worldaroundmee@gmail.com>
2025-05-27 17:07:52 +0700

    commit

--- components/FilterCard.tsx
+++ components/FilterCard.tsx
@@ -10,7 +10,7 @@ export default function FilterCard() {
 
   return (
     <div className="desktop:px-[165px] flex w-full justify-center px-[24px]">
-      <div className="desktop:h-full desktop:max-w-[1110px] shadow-primary/30 z-10 -mt-[34px] w-full max-w-[328px] rounded-lg bg-white shadow-2xl">
+      <div className="desktop:h-full desktop:max-w-[1110px] shadow-primary/30 z-10 -mt-[34px] w-full rounded-lg bg-white shadow-2xl">
         <div className="desktop:px-10 px-[20px] py-5">
           <div className="flex flex-row items-center justify-between gap-[20px]">
             <div className="flex flex-row flex-wrap gap-4">
